a3a31a21cdbdb0ea205d3b47237b0199bda72e8b
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2024-08-10 05:21:20 +0300

    Update custom_auth.py

--- app/auth/custom_auth.py
+++ app/auth/custom_auth.py
@@ -1,12 +1,27 @@
 from flask_appbuilder.security.sqla.manager import SecurityManager
-from flask import g
+from flask import g, request
 
 
 class CustomAuthSecurityManager(SecurityManager):
-    def authenticate(self, username, password):
-        if username == "admin" and password == "admin":
+    def authenticate(self, username=None, password=None):
+        if request.form and 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
             user = self.find_user(username=username)
-            if user:
+            if user and self._verify_password(password, user.password):
                 g.user = user
                 return True
-        return super().authenticate(username, password)
+
+        if not username and 'oauth_provider' in request.args:
+            oauth_provider = request.args['oauth_provider']
+            user = self.oauth_user_info()
+            if user:
+                g.user = self.find_user(email=user['email'])
+                return True
+
+        return False
+
+    def oauth_user_info(self):
+        provider = self.appbuilder.sm.oauth_remotes['github']
+        me = provider.get('user')
+        return me.json() if me.status == 200 else None
