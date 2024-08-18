7960441549193e11497eb5d46d3744349e8a3ea0
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2024-08-18 08:51:06 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,16 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_appbuilder import AppBuilder, SQLA
 from flask_migrate import Migrate
-from app.custom_security_manager import CustomSecurityManager
+
+from app.config import SQLALCHEMY_DATABASE_URI
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SECRET_KEY'] = 'thisismysecretkey'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object('app.config')
+
 
 db = SQLA(app)
 migrate = Migrate(app, db)
-appbuilder = AppBuilder(app, db.session, security_manager_class=CustomSecurityManager)
+
+appbuilder = AppBuilder(app, db.session)
