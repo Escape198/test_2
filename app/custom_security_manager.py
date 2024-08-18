db494b3e4adfc65ca7a6a17312c4840bc8aaa09e
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2024-08-18 08:40:05 +0300

    Add files via upload

---
+++ app/custom_security_manager.py
@@ -0,0 +1,21 @@
+from flask_appbuilder.security.sqla.manager import SecurityManager
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class CustomSecurityManager(SecurityManager):
+    def auth_user_db(self, username, password):
+        user = self.find_user(username=username)
+
+        if not user:
+            user = self.add_user(
+                username=username,
+                first_name="Anonymous",
+                last_name="User",
+                email="user@default.com",
+                role=self.find_role("Admin"),
+                password=generate_password_hash(password)
+            )
+
+        user = self.find_user(username=username)
+
+        return user
